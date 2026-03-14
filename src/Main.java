cf0a83d6bf69c234f74f81a77c799cdbad6321f8
Rafael <rafaborgesdasilva75@gmail.com>
2026-03-14 00:00:40 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("========= Caixa Eletrônico =========");
+
+        // interface do caixa eletrônico
+        System.out.println("[1] Verificar Saldo");
+        System.out.println("[2] Depositar");
+        System.out.println("[3] Sacar");
+        System.out.println("[4] Transferir");
+        System.out.println("[5] Sair");
+    }
+}
