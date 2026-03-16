3d28dd186d4eae1a86aa0acf58b178d8ceee1612
Rafael <rafaborgesdasilva75@gmail.com>
2026-03-15 22:39:24 -0300

    opçoes podem ser escolhidasa agora

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // esses 2 comentarios sao codigo para futura feature de login
+        //Scanner scanner = new Scanner(System.in);
+        //int input = scanner.nextInt();
 
+        while(true){
+            try{
+                Scanner input = new Scanner(System.in);
+                menu();
+                int opcao = input.nextInt();
+
+                // preencher if else com as funcoes de cada opcao do menu, usando os services e models criados
+                if (opcao == 1) {
+                    System.out.println("opcao 1");
+                } else if (opcao == 2) {
+                    System.out.println("opcao 2");
+                } else if (opcao == 3) {
+                    System.out.println("opcao 3");
+                } else if (opcao == 4) {
+                    System.out.println("opcao 4");
+                } else if (opcao == 5) {
+                    System.out.println(">> Saindo...");
+                    break;
+                }
+            } catch (Exception e){
+                System.out.println(">> ERRO | " + e.getMessage());
+            }
+
+        }
+    }
+
+    public static void menu() {
         System.out.println("========= Caixa Eletrônico =========");
 
         // interface do caixa eletrônico
