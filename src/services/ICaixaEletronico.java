b3260e3283eb6fbc1b2ba09eb54750fe19cac443
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-03-30 20:04:25 -0300

    Refactor CaixaEletronico to ICaixaEletronico interface

---
+++ src/services/ICaixaEletronico.java
@@ -0,0 +1,13 @@
+package services;
+
+public Interface ICaixaEletronico {
+    public String pegaValorTotalDisponivel();
+    
+    public String sacar(int valor);
+    
+    public String pegaRelatorioCedulas();
+    
+    public String reposicaoCedulas(int cedula, int quantidade);
+    
+    public String armazenaContaMinima();
+}
