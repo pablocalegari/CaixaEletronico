f4ea3f6326516fa3b28925886d16b2b505568764
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-03-30 20:44:36 -0300

    refatorado para ficar conforme os arquivos dados pelo Jadir

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,45 +1,28 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-// teste commit ignora 
-
-public class CaixaEletronico {
+public class CaixaEletronico implements ICaixaEletronico {
+    @Override
+    public String pegaValorTotalDisponivel(){}
+    
+    @Override
+    public String sacar(int valor){}
+    
+    @Override
+    public String pegaRelatorioCedulas(){}
+    
+    @Override
+    public String reposicaoCedulas(int cedula, int quantidade){}
+    
+    @Override
+    public String armazenaContaMinima(){}
+    
+    
     public static void main(String[] args) {
-        // esses 2 comentarios sao codigo para futura feature de login
-        //Scanner scanner = new Scanner(System.in);
-        //int input = scanner.nextInt();
-
-        int saldo = 100;
-        int bolso = 20;
-
-        while(true){
-            try{
-                Scanner input = new Scanner(System.in);
-                menu();
-                int opcao = input.nextInt();
-
-                // preencher if else com as funcoes de cada opcao do menu, usando os services e models criados
-                if (opcao == 1) {
-
-                } else if (opcao == 2) {
-
-                } else if (opcao == 3) {
-
-
-                } else if (opcao == 4) {
-                    
-                } else if (opcao == 5) {
-                    
-                    break;
-                }
-            } catch (Exception e){
-                System.out.println(">> ERRO | " + e.getMessage());
-            }
-
-        }
     }
-
-    public static void menu() {
+    
+    
+    /*public static void menu() {
         System.out.println("========= Caixa Eletrônico =========");
 
         // interface do caixa eletrônico
@@ -48,5 +31,6 @@ public class CaixaEletronico {
         System.out.println("[3] Sacar");
         System.out.println("[4] Transferir");
         System.out.println("[5] Sair");
-    }
+        }*/
+        
 }
