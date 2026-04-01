697a66c8918752436706f815574609287e1a151f
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-04-01 19:37:25 -0300

    feat: atualiza pegaRelatorioCedulas e inicializa matriz no main
    - Adicionada lógica de repetição (for) para percorrer a matriz de cédulas.
    - Inicializada a matriz 'cedulaRepositorio' com valores padrão no método main.
    - Ajustada a assinatura do método para receber a matriz como parâmetro.

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -6,11 +6,26 @@ public class CaixaEletronico implements ICaixaEletronico {
     public String pegaValorTotalDisponivel(){}
     
     @Override
-    public String sacar(int valor){}
+    public String sacar(int valor){
+        try{
+            
+        } catch (Exception e){
+            System.out.println("Um erro ocorreu: " + e);
+        }
+    }
     
     @Override
-    public String pegaRelatorioCedulas(){
-        int cedulaRepositorio = new int[6][2];
+    public String pegaRelatorioCedulas(int[][] cedulas){
+        // 01/04 - ainda nao testei issai
+        // percorre a matriz | 'i' é a linha, 'j' é a coluna
+        String relatorio;
+        for(int i = 0; i < 6; i++){
+            // vai printar o valor baseado na linha
+            // " linha i, valor na coluna 0, valor na coluna 1"
+            String linha = System.out.println("Valor Cedula: " + cedulas[i][0] + " | Quantidade: " + cedulas[i][1] + "\n");
+            relatorio.append(linha);
+        }
+        return relatorio.toString();
     }
     
     @Override
@@ -21,6 +36,17 @@ public class CaixaEletronico implements ICaixaEletronico {
     
     
     public static void main(String[] args) {
+        // isso aqui vai guardar as quantidades das cedulas
+        // primeira coluna é o valor a segunda é a quantidade
+        int[][] cedulaRepositorio = {{100, 100}, 
+                                    {50, 200}, 
+                                    {20, 300}, 
+                                    {10, 350}, 
+                                    {5, 450}, 
+                                    {2, 500}};
+                                    
+        
+        pegaRelatorioCedulas(cedulaRepositorio);
     }
     
     
