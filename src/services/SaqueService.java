ccad526872c7fe52f233f2c4beffdcdeefece121
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-04-01 20:56:35 -0300

    feat: adiciona validação de valores de saque permitidos
    - Implementado método isSaqueValido para bloquear valores ímpares impossíveis (1 e 3).

--- src/services/SaqueService.java
+++ src/services/SaqueService.java
@@ -1,5 +1,10 @@
 package services;
 
 public class SaqueService {
-
+  // verificar se funciona depois
+    public void int isSaqueValido(int valorSaque){
+        if (valorSaque == 1 || valorSaque == 3){
+            System.out.println("Valor Invalido: Não da pra sacar notas de 1 e 3");
+        }
+    }
 }
