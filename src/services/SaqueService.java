20f7474644b79892b8c8bc2584fd7902d761c3b7
Rafael <rafaborgesdasilva75@gmail.com>
2026-04-02 00:10:26 -0300

    feat: validaçao de saque testada

--- src/services/SaqueService.java
+++ src/services/SaqueService.java
@@ -1,10 +1,11 @@
 package services;
 
 public class SaqueService {
-  // verificar se funciona depois
-    public void int isSaqueValido(int valorSaque){
+    // verificar se funciona depois
+    public static boolean isSaqueValido(int valorSaque){
         if (valorSaque == 1 || valorSaque == 3){
             System.out.println("Valor Invalido: Não da pra sacar notas de 1 e 3");
         }
+        return false;
     }
 }
