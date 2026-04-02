072a283d1f77539b80480a44d4735d4405602c37
Rafael <rafaborgesdasilva75@gmail.com>
2026-04-02 00:11:46 -0300

    fix: retorno de valor

--- src/services/SaqueService.java
+++ src/services/SaqueService.java
@@ -5,7 +5,8 @@ public class SaqueService {
     public static boolean isSaqueValido(int valorSaque){
         if (valorSaque == 1 || valorSaque == 3){
             System.out.println("Valor Invalido: Não da pra sacar notas de 1 e 3");
+            return false;
         }
-        return false;
+        return true;
     }
 }
