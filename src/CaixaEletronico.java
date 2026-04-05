5f023a8a1c457148cae4afcf8c44418d77233567
Rafael <rafaborgesdasilva75@gmail.com>
2026-04-05 00:23:17 -0300

    feat: metodos "pegaValorTotalDisponivel" e "armazenaContaMinima" criados. matriz movidada do metodo main para atributo de classe

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -5,10 +5,25 @@ import java.io.IOException;
 
 
 public class CaixaEletronico implements ICaixaEletronico {
+    // isso aqui vai guardar as quantidades das cedulas
+    // primeira coluna é o valor a segunda é a quantidade
+    // criado como um atributo da classe para todos os metodos poderem usa-lo sem problema
+    private int[][] cedulaRepositorio = {{100, 100},
+                                        {50, 200},
+                                        {20, 300},
+                                        {10, 350},
+                                        {5, 450},
+                                        {2, 500}};
+    // atributo para o metodo armazenaContaMinima | numero de teste, vai mudar dps
+    private int minimoSaque = 200;
 
     @Override
     public String pegaValorTotalDisponivel(){
-        return "";
+        int valorSomado = 0;
+        for (int i = 0; i < cedulaRepositorio.length; i++){
+            valorSomado += cedulaRepositorio[i][1];
+        }
+        return "Valor total disponível: " + valorSomado;
     }
 
     @Override
@@ -23,14 +38,14 @@ public class CaixaEletronico implements ICaixaEletronico {
     }
 
     @Override
-    public String pegaRelatorioCedulas(int[][] cedulas) {
+    public String pegaRelatorioCedulas() {
         // 01/04 - ainda nao testei issai
         // percorre a matriz | 'i' é a linha, 'j' é a coluna
-        StringBuffer relatorio = new StringBuffer();
-        for (int i = 0; i < 6; i++) {
+        StringBuilder relatorio = new StringBuilder();
+        for (int i = 0; i < cedulaRepositorio.length; i++) {
             // vai printar o valor baseado na linha
             // " linha i, valor na coluna 0, valor na coluna 1"
-            String linha = "Valor Cedula: " + cedulas[i][0] + " | Quantidade: " + cedulas[i][1] + "\n";
+            String linha = "Valor Cedula: " + cedulaRepositorio[i][0] + " | Quantidade: " + cedulaRepositorio[i][1] + "\n";
             relatorio.append(linha);
         }
         return relatorio.toString();
@@ -42,24 +57,19 @@ public class CaixaEletronico implements ICaixaEletronico {
     }
 
     @Override
-    public String armazenaContaMinima(){
-        return "";
+    public String armazenaContaMinima(int minimo){
+        // verifica se o valor passado é menor que zero
+        if (minimo < 0){
+            return "Valor minimo de saque não pode ser negativo.";
+        }
+
+        // se nao for, atualiza o valor do minimoSaque e retorna uma mensagem de sucesso
+        this.minimoSaque = minimo;
+        return "Valor mínimo de saque atualizado para: " + minimo;
     }
     
     
     public void main(String[] args) {
-        // isso aqui vai guardar as quantidades das cedulas
-        // primeira coluna é o valor a segunda é a quantidade
-        int[][] cedulaRepositorio = {{100, 100}, 
-                                    {50, 200}, 
-                                    {20, 300}, 
-                                    {10, 350}, 
-                                    {5, 450}, 
-                                    {2, 500}};
-
-        //System.out.println(pegaRelatorioCedulas(cedulaRepositorio));
-
-
 
     }
     
