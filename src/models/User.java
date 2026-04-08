32a0abb05b154b6bf21c0a709c1a20bcdbf19307
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-04-08 18:50:59 -0300

    adicionado getters e setters para a classe do usuario

--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,25 @@
 package models;
 
-public class UserModel {
+public class User {
+    private String name;
+    private String senha;
+    private int saldo;
+    
+    // 08/04 rafael: talvez eu tire essa agencia depois
+    private String agencia;
+    
+    public User(String name, String senha, int saldo, String agencia){
+        this.name = name;
+        this.senha = senha;
+        this.saldo = saldo;
+        this.agencia = agencia;
+    }
+    
+    public int getSaldo(){
+        return this.saldo;
+    }
+    
+    public void setSaldo(int saldo){
+        this.saldo = saldo;
+    }
 }
